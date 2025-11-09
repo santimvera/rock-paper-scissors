b9114e63f2576de3f9213b6982db1cd3054b34b5
santimvera <87710111+santimvera@users.noreply.github.com>
2025-11-09 13:25:26 +0100

    Refactor the testing file

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,10 +1,20 @@
+const choices = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
     let result;
-    const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     result = choices[randomIndex];
     return result;
 }
 
+function getHumanChoice() {
+    let result;
+    result = "rock";
+    return result;
+}
 
-module.exports = getComputerChoice;
+
+module.exports = {
+    getComputerChoice,
+    getHumanChoice
+};
--- rockPaperScissors.spec.js
+++ rockPaperScissors.spec.js
@@ -1,9 +1,16 @@
-const getComputerChoice = require('./rockPaperScissors')
+const rockPaperScissors = require('./rockPaperScissors')
+const validChoices = ['rock', 'paper', 'scissors'];
 
 describe('rockPaperScissors', () => {
     test('computer choices should return valid option', () => {
-        const validChoices = ['rock', 'paper', 'scissors'];
-        const result = getComputerChoice();
+        const result = rockPaperScissors.getComputerChoice();
+        expect(validChoices).toContain(result);
+    });
+});
+
+describe('rockPaperScissors', () => {
+    test.skip('human choices should return valid option', () => {
+        const result = rockPaperScissors.getHumanChoice("paper");
         expect(validChoices).toContain(result);
     });
 });
