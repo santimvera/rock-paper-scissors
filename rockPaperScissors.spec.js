05a6c11e608b0b14a1223c2d1d715f9c872f5ead
santimvera <87710111+santimvera@users.noreply.github.com>
2025-11-09 13:06:54 +0100

    Setup the test environment

---
+++ rockPaperScissors.spec.js
@@ -0,0 +1,9 @@
+const { getComputerChoice } = require('./rockPaperScissors')
+
+describe('rockPaperScissors', () => {
+  test('computer choices should return valid option', () => {
+    const validChoices = ['rock', 'paper', 'scissors'];
+    const result = getComputerChoice();
+    expect(validChoices).toContain(result);
+  });
+});
